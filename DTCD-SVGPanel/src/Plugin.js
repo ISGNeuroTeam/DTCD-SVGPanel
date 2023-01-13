58d8845118fd53be42102e39120c7ae467d28869
Alexandr-Bragin <abragin@isgneuro.com>
2023-01-13 19:20:09 +0900

    Added data render

--- DTCD-SVGPanel/src/Plugin.js
+++ DTCD-SVGPanel/src/Plugin.js
@@ -1,13 +1,30 @@
 import {
   PanelPlugin,
   LogSystemAdapter,
+  InteractionSystemAdapter,
+  EventSystemAdapter,
+  StorageSystemAdapter,
+  DataSourceSystemAdapter,
 } from './../../DTCD-SDK';
 
 import pluginMeta from './Plugin.Meta';
 import PluginComponent from './PluginComponent.vue';
 
 export class SVGPanel extends PanelPlugin {
+  #id;
+  #guid;
   #vueComponent;
+  #logSystem;
+  #eventSystem;
+  #dataSourceSystem;
+  #dataSourceSystemGUID;
+  #storageSystem;
+
+  #config = {
+    isSettingsFromDatasource: false,
+    ...this.defaultConfig,
+    dataSource: '',
+  };
 
   static getRegistrationMeta() {
     return pluginMeta;
@@ -16,14 +33,27 @@ export class SVGPanel extends PanelPlugin {
   constructor(guid, selector) {
     super();
 
-    const logSystem = new LogSystemAdapter('0.5.0', guid, pluginMeta.name);
+    this.#guid = guid;
+    this.#id = `${pluginMeta.name}[${guid}]`;
+
+    this.#logSystem = new LogSystemAdapter('0.5.0', guid, pluginMeta.name);
+    const interactionSystem = new InteractionSystemAdapter('0.4.0');
+    this.#eventSystem = new EventSystemAdapter('0.4.0', guid);
+    this.#eventSystem.registerPluginInstance(this);
+    this.#storageSystem = new StorageSystemAdapter('0.5.0');
+
+    this.#dataSourceSystem = new DataSourceSystemAdapter('0.2.0');
+    this.#dataSourceSystemGUID = this.getGUID(
+      this.getSystem('DataSourceSystem', '0.2.0')
+    );
 
     const { default: VueJS } = this.getDependence('Vue');
 
     const view = new VueJS({
       data: () => ({
         guid,
-        logSystem,
+        logSystem: this.#logSystem,
+        interactionSystem,
       }),
       render: h => h(PluginComponent),
     }).$mount(selector);
@@ -31,17 +61,95 @@ export class SVGPanel extends PanelPlugin {
     this.#vueComponent = view.$children[0];
   }
 
-  setPluginConfig(config = {}) {
-    const { title } = config;
-
-    if (typeof title !== 'undefined') {
-      this.#vueComponent.title = title;
+  setVueComponentPropValue(prop, value) {
+    const vueFields = Object.keys(this.#vueComponent);
+    if (vueFields.includes(prop)) {
+      this.#vueComponent[prop] = value;
     }
+  }
+
+  setPluginConfig(config = {}) {
+    this.#logSystem.debug(`Set new config to ${this.#id}`);
+    this.#logSystem.info(`Set new config to ${this.#id}`);
+
+    // const configProps = Object.keys(this.#config);
+
+    for (const [prop, value] of Object.entries(config)) {
+      // if (!configProps.includes(prop)) continue;
+
+      if (prop !== 'dataSource') {
+        this.setVueComponentPropValue(prop, value);
+      } else if (value) {
+        if (this.#config[prop]) {
+          this.#logSystem.debug(
+            `Unsubscribing ${this.#id} from DataSourceStatusUpdate({ dataSource: ${this.#config[prop]}, status: success })`
+          );
+          this.#eventSystem.unsubscribe(
+            this.#dataSourceSystemGUID,
+            'DataSourceStatusUpdate',
+            this.#guid,
+            'processDataSourceEvent',
+            { dataSource: this.#config[prop], status: 'success' },
+          );
+        }
+
+        const dsNewName = value;
+
+        this.#logSystem.debug(
+          `Subscribing ${this.#id} for DataSourceStatusUpdate({ dataSource: ${dsNewName}, status: success })`
+        );
+
+        this.#eventSystem.subscribe(
+          this.#dataSourceSystemGUID,
+          'DataSourceStatusUpdate',
+          this.#guid,
+          'processDataSourceEvent',
+          { dataSource: dsNewName, status: 'success' },
+        );
+
+        const ds = this.#dataSourceSystem.getDataSource(dsNewName);
+
+        if (ds && ds.status === 'success') {
+          const data = this.#storageSystem.session.getRecord(dsNewName);
+          this.loadData(data);
+        }
+      }
+
+      this.#config[prop] = value;
+      this.#logSystem.debug(`${this.#id} config prop value "${prop}" set to "${value}"`);
+    }
+  }
+
+  loadData(data) {
+    this.#vueComponent.setDataset(data);
+  }
+
+  processDataSourceEvent(eventData) {
+    const { dataSource, status } = eventData;
+    const data = this.#storageSystem.session.getRecord(dataSource);
+    this.#logSystem.debug(
+      `${this.#id} process DataSourceStatusUpdate({ dataSource: ${dataSource}, status: ${status} })`
+    );
+
+    if (this.#config.isSettingsFromDatasource) {
+      if (data.length > 0) {
+        if (data[0]?.title) {
+          this.setVueComponentPropValue('title', data[0]?.title);
+        }
+        if (data[0]?.adaptiveImg) {
+          this.setVueComponentPropValue('adaptiveImg', data[0]?.adaptiveImg);
+        }
+      }
+    }
+
+    this.loadData(data);
   }
 
   getPluginConfig() {
     const config = {
+      ...this.#config,
       title: this.#vueComponent.title,
+      adaptiveImg: this.#vueComponent.adaptiveImg,
     };
     return config;
   }
@@ -54,8 +162,31 @@ export class SVGPanel extends PanelPlugin {
     return {
       fields: [
         {
-          component: 'title',
-          propValue: 'Общие настройки',
+          component: 'input',
+          propName: 'title',
+          attrs: {
+            label: 'Заголовок панели',
+          },
+        },
+        {
+          component: 'switch',
+          propName: 'adaptiveImg',
+          attrs: {
+            label: 'Адаптировать картинку по ширине',
+            checked: true,
+          },
+        },
+        {
+          component: 'divider',
+        },
+        {
+          component: 'datasource',
+          propName: 'dataSource',
+          attrs: {
+            label: 'Выберите источник данных',
+            placeholder: 'Выберите значение',
+            required: true,
+          },
         },
       ],
     };
--- DTCD-SVGPanel/src/PluginComponent.vue
+++ DTCD-SVGPanel/src/PluginComponent.vue
@@ -21,26 +21,102 @@ export default {
   data() {
     return {
       title: 'SVG',
+      adaptiveImg: true,
+      svgFileName: '',
+      dataset: [],
     };
   },
   mounted() {
-    if (this.$refs.csvg) {
-      this.getSVG('well_dark_fixed.svg');
-    }
+    // ищем название картинки для загрузки, а после расставляем данные в HTML-теги
+    this.dataset.find((dataSetItem) => {
+      if (dataSetItem.svg_filename) {
+        if (dataSetItem.svg_filename !== this.svgFileName) {
+          this.svgFileName = dataSetItem.svg_filename;
+          this.getSVG(dataSetItem.svg_filename);
+          this.renderDataSet();
+          return true;
+        }
+      } else {
+        return false;
+      }
+    });
+  },
+  watch: {
+    adaptiveImg(newValue) {
+      if (this.$refs.svg) {
+        this.$refs.svg.style.width = newValue ? '100%' : '';
+        this.$refs.svg.style.height = newValue ? 'auto' : '';
+      }
+    },
+    dataset(newDataSet) {
+      this.renderDataSet(newDataSet);
+    },
   },
   methods: {
     async getSVG(filename) {
+      if (!filename) return false;
+
       const response = await this.$root.interactionSystem.GETRequest(`tmp_images/${filename}`);
-      if (response.status == 200 && response.data.indexOf('<svg') != -1) {
+      if (response?.data?.indexOf('<svg') != -1) {
         this.$refs.csvg.innerHTML = response.data;
-        const svg = this.$refs.csvg.querySelector('svg');
-        if (svg) {
-          svg.removeAttribute('width');
-          svg.removeAttribute('height');
-          svg.style.width = '100%';
+        this.$refs.svg = this.$refs.csvg.querySelector('svg');
+
+        if (this.$refs.svg && this.adaptiveImg) {
+          this.$refs.svg.style.width = '100%';
+          this.$refs.svg.style.height = 'auto';
         }
       }
     },
+
+    setDataset(data) {
+      if (!Array.isArray(data)) return false;
+      
+      data.forEach((dataItem) => {
+        if (dataItem.svg_filename && dataItem.svg_filename !== this.svgFileName) {
+          this.svgFileName = dataItem.svg_filename;
+          this.getSVG(dataItem.svg_filename);
+        }
+        
+        let findedDataSetItem = this.dataset.find((dataSetItem) => {
+          return (dataItem.id === dataSetItem.id);
+        });
+
+        if (findedDataSetItem) {
+          Object.assign(findedDataSetItem, dataItem);
+        } else {
+          this.dataset.push({...dataItem});
+        }
+      });
+    },
+
+    renderDataSet(dataSetParam) {
+      const dataset = (Array.isArray(dataSetParam)) ? dataSetParam : this.dataset;
+
+      let intervalCounter = 0;
+      const interval = setInterval(() => {
+        intervalCounter++;
+        if (intervalCounter > 100) {
+          clearInterval(interval);
+          return;
+        }
+
+        if (this.$refs.svg) {
+          clearInterval(interval);
+
+          dataset.forEach((dataSetItem) => {
+            if (!dataSetItem.tagInSVG) {
+              dataSetItem.tagInSVG = this.$refs.svg.querySelector('#' + dataSetItem.id);
+              if (!dataSetItem.tagInSVG) return;
+            }
+
+            dataSetItem.tagInSVG.innerHTML = dataSetItem.tag_value;
+            dataSetItem.fill
+              ? dataSetItem.tagInSVG.setAttribute('fill', dataSetItem.fill)
+              : dataSetItem.tagInSVG.removeAttribute('fill');
+          });
+        }
+      }, 100);
+    }
   },
 };
 </script>
